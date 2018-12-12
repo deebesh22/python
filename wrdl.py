6098e0addec71bfd1bba3a5ffa3a39a957556895
deebesh22 <44742800+deebesh22@users.noreply.github.com>
2018-12-12 14:37:43 +0530

    Add files via upload

---
+++ wrdl.py
@@ -0,0 +1,14 @@
+class Textreader :
+text="hi hello world"
+    lines="1"
+    wordcount="3"
+    def __init__(self,a,b)
+      t=open("article.txt","r")
+      self.text=a
+      self.lines=b
+      t.close()
+    def display(self)
+      print(text)
+    def wordlist(self)
+      return hello
+    def getWC
